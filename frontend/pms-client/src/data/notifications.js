06799fa931075ff1443b77f55849276385cd0787
SebastianOpiyo <sebamopale@gmail.com>
2021-07-11 18:45:41 +0300

    Frontend cleanup and Enhancement

---
+++ frontend/pms-client/src/data/notifications.js
@@ -0,0 +1,54 @@
+
+import Profile1 from "../assets/img/team/profile-picture-1.jpg"
+import Profile2 from "../assets/img/team/profile-picture-2.jpg"
+import Profile3 from "../assets/img/team/profile-picture-3.jpg"
+import Profile4 from "../assets/img/team/profile-picture-4.jpg"
+import Profile5 from "../assets/img/team/profile-picture-5.jpg"
+
+export default [
+    {
+        "id": 1,
+        "read": false,
+        "image": Profile1,
+        "sender": "Jose Natiminya",
+        "time": "1 hour ago",
+        "link": "#",
+        "message": `Send a Parcel "A HP model, 2344 computer.`
+    },
+    {
+        "id": 2,
+        "read": false,
+        "image": Profile2,
+        "sender": "Neil Simiyu",
+        "time": "2 hrs ago",
+        "link": "#",
+        "message": `You've been send a coinsignement "10 boxes of battery".`
+    },
+    {
+        "id": 3,
+        "read": false,
+        "image": Profile3,
+        "sender": "Rebecca Olwali",
+        "time": "5 hrs ago",
+        "link": "#",
+        "message": `Send a parcel "Letter to Joash Omollo".`
+    },
+    {
+        "id": 4,
+        "read": true,
+        "image": Profile4,
+        "sender": "Joseph Gakiru",
+        "time": "1 day ago",
+        "link": "#",
+        "message": `New Parcel: "Glasses for housing."`
+    },
+    {
+        "id": 5,
+        "read": true,
+        "image": Profile5,
+        "sender": "Jose Lihanda",
+        "time": "2 days ago",
+        "link": "#",
+        "message": `New message: "Send a box of text books."`
+    },
+]
